4264ae5b7ef20600f91244e6aea6faa95f2e3618
Miguel <misanma484@hotmail.com>
2021-09-21 08:18:11 +0200

    Merge de las ramas

--- AE1/src/AE1_3/AE1_3.java
+++ AE1/src/AE1_3/AE1_3.java
@@ -8,19 +8,21 @@ public class AE1_3 {
 
 	public static void main(String[] args) {
 		
-		int numero = 10;
-		System.out.println(sumaPares(numero));
+		int numero = 6;
+		int consola = sumaPares(numero);
+		System.out.println(consola);
 
 	}
 	
-	public static int sumaPares(int numero) {
+	public static int sumaPares(int num) {
 		int valor = 0;
 		
-		for(int i=0; i<numero; i++) {
+		for(int i=0; i<=num; i++) {
 			
-			if(numero %2 == 0) {
-				valor = valor + i;
+			if(num %2 == 0) {
+				valor += i;
 			}
+			System.out.println(valor);
 		}
 		
 		return valor;		
